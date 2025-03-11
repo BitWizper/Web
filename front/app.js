5205d6fe58bc2a72c04108ddd5303c774bbc6907
mily <emisalazarc06@gmail.com>
2025-03-11 08:24:24 -0600

    se modifico a que se vea primero el home en lugar dle inicio de sesión

--- front/app.js
+++ front/app.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
+
+// Ruta principal para home.html (antes de servir archivos estáticos)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'components', 'home.html'));
+});
 
 // Servir archivos estáticos (HTML, CSS, JS) desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Ruta principal para index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 // Ruta para login.html
 app.get('/login', (req, res) => {
@@ -27,5 +26,5 @@ const PORT = process.env.PORT || 4000;
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-  console.log('Servidor de frontend corriendo en http://localhost:${PORT}');
+  console.log(`Servidor de frontend corriendo en http://localhost:${PORT}`);
 });
